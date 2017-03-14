asad-sangla/sqlopsstudio

commit 1f5ff82c3c9fdedf99d940a5e91adca0749b5e5f

Query Execution Support through DMP to Service Layer (#252)
- DMP implementation for a QueryProvider
- Replaced mock results in QueryRunner with actual E2E plumb through of events
- Moved all Carbon events to a carbon.d.ts file. Note: can change file name / location and the namespace as needed.

src/sql/parts/query/execution/queryModel.ts
@@ -4,12 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 
 import QueryRunner from 'sql/parts/query/execution/queryRunner';
-import { ISelectionData } from 'sql/parts/connection/node/interfaces';
 import { DataService } from 'sql/parts/grid/services/dataService';
 import { ISlickRange } from 'angular2-slickgrid';
-import { ResultSetSubset } from 'sql/parts/query/execution/contracts/queryExecute';
 import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
 import Event from 'vs/base/common/event';
+import { ISelectionData, ResultSetSubset } from 'data';
 
 export const SERVICE_ID = 'queryModelService';
 
@@ -36,7 +35,5 @@ export interface IQueryModelService {
 	showError(uri: string, message: string): void;
 
 	onRunQueryStart: Event<string>;
-	onRunQueryComplete: Event<string>;
-
-	TEST_sendDummyQueryEvents(uri: string): void;
+onRunQueryComplete: Event<string>;
 }

src/sql/parts/query/execution/queryModelService.ts
@@ -8,13 +8,15 @@
 import Constants = require('sql/parts/connection/node/constants');
 import Utils = require('sql/parts/connection/node/utils');
 import QueryRunner from 'sql/parts/query/execution/queryRunner';
-import { ISelectionData } from 'sql/parts/connection/node/interfaces';
 import { DataService } from 'sql/parts/grid/services/dataService';
 import { ISlickRange } from 'angular2-slickgrid';
-import { ResultSetSubset } from 'sql/parts/query/execution/contracts/queryExecute';
 import { IQueryModelService } from 'sql/parts/query/execution/queryModel';
+import { IQueryManagementService } from 'sql/parts/query/common/queryManagement';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { IMessageService } from 'vs/platform/message/common/message';
+import Severity from 'vs/base/common/severity';
 import Event, { Emitter } from 'vs/base/common/event';
+import { ISelectionData, ResultSetSubset } from 'data';
 
 interface QueryEvent {
 	type: string;
@@ -33,11 +35,10 @@ class QueryInfo {
 	// via the data service will be lost.
 	public dataServiceReady: boolean;
 
-	constructor() {
+	constructor () {
 		this.dataServiceReady = false;
 		this.queryEventQueue = [];
 	}
-
 }
 
 /**
@@ -57,6 +58,8 @@ export class QueryModelService implements IQueryModelService {
 	// CONSTRUCTOR /////////////////////////////////////////////////////////
 	constructor(
 		@IInstantiationService private _instantiationService: IInstantiationService,
+		@IQueryManagementService private _queryManagementService: IQueryManagementService,
+		@IMessageService private _messageService: IMessageService
 	) {
 		this._queryInfoMap = new Map<string, QueryInfo>();
 		this._onRunQueryStart = new Emitter<string>();
@@ -64,11 +67,6 @@ export class QueryModelService implements IQueryModelService {
 	}
 
 	// IQUERYMODEL /////////////////////////////////////////////////////////
-
-	public TEST_sendDummyQueryEvents(uri: string): void {
-		this._queryInfoMap.get(uri).queryRunner.TEST_setupRunQuery();
-	}
-
 	public getDataService(uri: string): DataService {
 		let dataService = this._queryInfoMap.get(uri).dataService;
 		if (!dataService) {
@@ -94,7 +92,7 @@ export class QueryModelService implements IQueryModelService {
 	 * Get more data rows from the current resultSets from the service layer
 	 */
 	public getRows(uri: string, rowStart: number, numberOfRows: number, batchId: number, resultId: number): Thenable<ResultSetSubset> {
-		return this._queryInfoMap.get(uri).queryRunner.getRows(rowStart, numberOfRows, batchId, resultId).then(results => {
+		return this._queryInfoMap.get(uri).queryRunner.getQueryRows(rowStart, numberOfRows, batchId, resultId).then(results => {
 			return results.resultSubset;
 		});
 	}
@@ -153,7 +151,7 @@ export class QueryModelService implements IQueryModelService {
 		} else {
 			// We do not have a query runner for this editor, so create a new one
 			// and map it to the results uri
-			queryRunner = new QueryRunner(uri, title);
+			queryRunner = this._instantiationService.createInstance(QueryRunner, uri, title);
 			queryRunner.eventEmitter.on('resultSet', (resultSet) => {
 				this._fireQueryEvent(uri, 'resultSet', resultSet);
 			});
@@ -212,8 +210,10 @@ export class QueryModelService implements IQueryModelService {
 
 		// Cancel the query
 		queryRunner.cancelQuery().then(success => undefined, error => {
-			// On error, show error message
-			// TODO: Canceling the query failed: {0}
+			// On error, show error message and notify that the query is complete so that buttons and other status indicators
+			// can be correct
+			this._messageService.show(Severity.Error, Utils.formatString(Constants.msgCancelQueryFailed, error));
+			this._fireQueryEvent(queryRunner.uri, 'complete', 0);
 		});
 
 	}

src/sqltest/parts/query/editor/queryEditor.test.ts
@@ -7,12 +7,14 @@
 
 import { EditorDescriptorService } from 'sql/parts/query/editor/editorDescriptorService';
 import { InstantiationService } from 'vs/platform/instantiation/common/instantiationService';
+import { IMessageService } from 'vs/platform/message/common/message';
 import { IEditorDescriptor, EditorInput } from 'vs/workbench/common/editor';
 import { TPromise } from 'vs/base/common/winjs.base';
 import URI from 'vs/base/common/uri';
 import { QueryResultsInput } from 'sql/parts/query/common/queryResultsInput';
 import { QueryEditor } from 'sql/parts/query/editor/queryEditor';
 import { QueryModelService } from 'sql/parts/query/execution/queryModelService';
+import { IQueryManagementService } from 'sql/parts/query/common/queryManagement';
 import { QueryInput } from 'sql/parts/query/common/queryInput';
 import { UntitledEditorInput } from 'vs/workbench/common/editor/untitledEditorInput';
 import { Builder } from 'vs/base/browser/builder';
@@ -23,6 +25,8 @@ import * as assert from 'assert';
 suite('SQL QueryEditor Tests', () => {
 	let queryModelService: QueryModelService;
 	let instantiationService: TypeMoq.Mock<InstantiationService>;
+	let queryManagementService: TypeMoq.Mock<IQueryManagementService>;
+	let messageService: TypeMoq.Mock<IMessageService>;
 	let editorDescriptorService: TypeMoq.Mock<EditorDescriptorService>;
 	let queryInput: QueryInput;
 	let queryInput2: QueryInput;
@@ -78,7 +82,7 @@ suite('SQL QueryEditor Tests', () => {
 		queryInput2 = new QueryInput('second', 'second', fileInput2, queryResultsInput2, undefined);
 
 		// Create a QueryModelService
-		queryModelService = new QueryModelService(instantiationService.object);
+		queryModelService = new QueryModelService(instantiationService.object, queryManagementService.object, messageService.object);
 	});
 
 	test('createEditor creates only the taskbar', (done) => {

build/gulpfile.vscode.js
@@ -262,6 +262,7 @@ function packageTask(platform, arch, opts) {
 
 		// TODO the API should be copied to `out` during compile, not here
 		const api = gulp.src('src/vs/vscode.d.ts').pipe(rename('out/vs/vscode.d.ts'));
+		const dataApi = gulp.src('src/vs/data.d.ts').pipe(rename('out/sql/data.d.ts'));
 
 		const depsSrc = _.flatten(dependencies
 			.map(function (d) { return ['node_modules/' + d + '/**', '!node_modules/' + d + '/**/{test,tests}/**']; }));
@@ -280,6 +281,7 @@ function packageTask(platform, arch, opts) {
 			productJsonStream,
 			license,
 			api,
+			dataApi,
 			sources,
 			deps
 		);

dataprotocol-node/client/src/codeConverter.ts
@@ -5,6 +5,7 @@
 'use strict';
 
 import * as code from 'vscode';
+import * as data from 'data';
 import * as ls from 'dataprotocol-languageserver-types';
 import * as proto from './protocol';
 import * as is from './utils/is';
@@ -61,9 +62,9 @@ export interface Converter {
 
 	asDocumentLinkParams(textDocument: code.TextDocument): proto.DocumentLinkParams;
 
-	asConnectionParams(connectionUri: string, connectionInfo: code.ConnectionInfo) : proto.ConnectParams;
+	asConnectionParams(connectionUri: string, connectionInfo: data.ConnectionInfo) : proto.ConnectParams;
 
-	asCapabilitiesParams(client: code.DataProtocolClientCapabilities): proto.CapabiltiesDiscoveryParams;
+	asCapabilitiesParams(client: data.DataProtocolClientCapabilities): proto.CapabiltiesDiscoveryParams;
 
 	asMetadataQueryParams(connectionUri: string) : ls.MetadataQueryParams;
 
@@ -310,7 +311,7 @@ export function createConverter(uriConverter?: URIConverter): Converter {
 		};
 	}
 
-	function asCapabilitiesParams(client: code.DataProtocolClientCapabilities): proto.CapabiltiesDiscoveryParams {
+	function asCapabilitiesParams(client: data.DataProtocolClientCapabilities): proto.CapabiltiesDiscoveryParams {
 		let params:  proto.CapabiltiesDiscoveryParams = {
 			hostName: client.hostName,
 			hostVersion: client.hostVersion
@@ -318,7 +319,7 @@ export function createConverter(uriConverter?: URIConverter): Converter {
 		return params;
 	}
 
-	function asConnectionParams(connUri: string, connInfo: code.ConnectionInfo) : proto.ConnectParams {
+	function asConnectionParams(connUri: string, connInfo: data.ConnectionInfo) : proto.ConnectParams {
 		return {
 			ownerUri: connUri,
 			connection: {

dataprotocol-node/client/src/main.ts
@@ -14,11 +14,17 @@ import {
 		CompletionItem as VCompletionItem, CompletionList as VCompletionList, SignatureHelp as VSignatureHelp, Definition as VDefinition, DocumentHighlight as VDocumentHighlight,
 		SymbolInformation as VSymbolInformation, CodeActionContext as VCodeActionContext, Command as VCommand, CodeLens as VCodeLens,
 		FormattingOptions as VFormattingOptions, TextEdit as VTextEdit, WorkspaceEdit as VWorkspaceEdit, MessageItem,
-		DocumentLink as VDocumentLink, ConnectionInfo, ConnectionInfoSummary, dataprotocol,
-		DataProtocolProvider, ConnectionProvider, DataProtocolServerCapabilities as VDataProtocolServerCapabilities,
-		DataProtocolClientCapabilities, CapabilitiesProvider, MetadataProvider, ScriptingProvider, ProviderMetadata,
-		ScriptingResult
+		DocumentLink as VDocumentLink
+
 } from 'vscode';
+
+import {
+		ConnectionInfo, ConnectionInfoSummary, dataprotocol, DataProtocolProvider, ConnectionProvider,
+		DataProtocolServerCapabilities as VDataProtocolServerCapabilities,
+		DataProtocolClientCapabilities, CapabilitiesProvider, MetadataProvider,
+		ScriptingProvider, ProviderMetadata, ScriptingResult,
+		QueryProvider, QueryCancelResult as VQueryCancelResult
+} from 'data';
 
 import {
 		Message,
@@ -40,6 +46,7 @@ import {
 		FormattingOptions, DocumentLink,
 		ConnectionCompleteParams, IntelliSenseReadyParams,
 		ConnectionProviderOptions, DataProtocolServerCapabilities,
+		ISelectionData, QueryExecuteBatchNotificationParams,
 		MetadataQueryParams, MetadataQueryResult,
 		ScriptingSelectParams, ScriptingSelectResult
 } from 'dataprotocol-languageserver-types';
@@ -68,8 +75,17 @@ import {
 		DocumentOnTypeFormattingRequest, DocumentOnTypeFormattingParams,
 		RenameRequest, RenameParams,
 		DocumentLinkRequest, DocumentLinkResolveRequest, DocumentLinkParams,
-		CapabiltiesDiscoveryRequest, ConnectionRequest, DisconnectRequest, DisconnectParams,
-		ConnectionCompleteNotification, IntelliSenseReadyNotification, MetadataQueryRequest, ScriptingSelectRequest
+		CapabiltiesDiscoveryRequest,
+		ConnectionRequest, ConnectParams,
+		DisconnectRequest, DisconnectParams,
+		ConnectionCompleteNotification, IntelliSenseReadyNotification,
+		MetadataQueryRequest, ScriptingSelectRequest,
+		QueryCancelRequest, QueryCancelResult, QueryCancelParams,
+		QueryExecuteRequest, QueryExecuteSubsetResult, QueryExecuteSubsetParams,
+		QueryExecuteBatchStartNotification, QueryExecuteBatchCompleteNotification, QueryExecuteCompleteNotification,
+		QueryExecuteMessageNotification, QueryDisposeParams, QueryDisposeRequest, QueryDisposeResult, QueryExecuteCompleteNotificationResult,
+		QueryExecuteMessageParams, QueryExecuteParams, QueryExecuteResult, QueryExecuteResultSetCompleteNotification, QueryExecuteResultSetCompleteNotificationParams,
+		QueryExecuteSubsetRequest
 } from './protocol';
 
 import * as c2p from './codeConverter';
@@ -179,7 +195,7 @@ function createConnection(input: any, output: any, errorHandler: ConnectionError
 		onDiagnostics: (handler: NotificationHandler<PublishDiagnosticsParams>) => connection.onNotification(PublishDiagnosticsNotification.type, handler),
 
 		dispose: () => connection.dispose()
-	}
+	};
 
 	return result;
 }
@@ -1270,7 +1286,7 @@ export class LanguageClient {
 				return self.doSendRequest(connection, CapabiltiesDiscoveryRequest.type, self._c2p.asCapabilitiesParams(client), undefined).then(
 					self._p2c.asServerCapabilities,
 					(error) => {
-						this.logFailedRequest(ConnectionRequest.type, error);
+						self.logFailedRequest(ConnectionRequest.type, error);
 						return Promise.resolve([]);
 					}
 				);
@@ -1286,7 +1302,7 @@ export class LanguageClient {
 						return result;
 					},
 					(error) => {
-						this.logFailedRequest(ConnectionRequest.type, error);
+						self.logFailedRequest(ConnectionRequest.type, error);
 						return Promise.resolve(false);
 					}
 				);
@@ -1302,7 +1318,7 @@ export class LanguageClient {
 						return result;
 					},
 					(error) => {
-						this.logFailedRequest(DisconnectRequest.type, error);
+						self.logFailedRequest(DisconnectRequest.type, error);
 						return Promise.resolve(false);
 					}
 				);
@@ -1323,6 +1339,103 @@ export class LanguageClient {
 			registerOnIntelliSenseCacheComplete(handler: (connectionUri: string) => any) {
 				connection.onNotification(IntelliSenseReadyNotification.type, (params: IntelliSenseReadyParams) => {
 					handler(params.ownerUri);
+				});
+			}
+		};
+
+		let queryProvider: QueryProvider = {
+			handle: -1,
+			queryType: 'MSSQL',
+			cancelQuery(ownerUri: string): Thenable<QueryCancelResult> {
+				let params: QueryCancelParams = { ownerUri: ownerUri };
+				return self.doSendRequest(connection, QueryCancelRequest.type, params, undefined).then(
+					(result) => {
+						return result;
+					},
+					(error) => {
+						self.logFailedRequest(QueryCancelRequest.type, error);
+						return Promise.resolve([]);
+					}
+				);
+			},
+
+			runQuery(ownerUri: string, selection: ISelectionData): Thenable<void> {
+				let params: QueryExecuteParams = {ownerUri: ownerUri, querySelection: selection };
+				return self.doSendRequest(connection, QueryExecuteRequest.type, params, undefined).then(
+					(result) => {
+						return undefined;
+					},
+					(error) => {
+						self.logFailedRequest(QueryExecuteRequest.type, error);
+						return Promise.resolve([]);
+					}
+				);
+			},
+
+			getQueryRows(rowData: QueryExecuteSubsetParams): Thenable<QueryExecuteSubsetResult> {
+				return self.doSendRequest(connection, QueryExecuteSubsetRequest.type, rowData, undefined).then(
+					(result) => {
+						return result;
+					},
+					(error) => {
+						self.logFailedRequest(QueryExecuteSubsetRequest.type, error);
+						return Promise.resolve([]);
+					}
+				);
+			},
+
+			disposeQuery(ownerUri: string): Thenable<void>{
+				let params: QueryDisposeParams = { ownerUri: ownerUri };
+				return self.doSendRequest(connection, QueryDisposeRequest.type, params, undefined).then(
+					(result) => {
+						return undefined;
+					},
+					(error) => {
+						self.logFailedRequest(QueryDisposeRequest.type, error);
+						return Promise.resolve([]);
+					}
+				);
+			},
+
+			registerOnQueryComplete(handler: (result: QueryExecuteCompleteNotificationResult) => any) {
+				connection.onNotification(QueryExecuteCompleteNotification.type, (params: QueryExecuteCompleteNotificationResult) => {
+					handler({
+						ownerUri: params.ownerUri,
+						batchSummaries: params.batchSummaries});
+				});
+			},
+
+			registerOnBatchStart(handler: (batchInfo: QueryExecuteBatchNotificationParams) => any) {
+				connection.onNotification(QueryExecuteBatchStartNotification.type, (params: QueryExecuteBatchNotificationParams) => {
+					handler({
+						batchSummary: params.batchSummary,
+						ownerUri: params.ownerUri
+					});
+				});
+			},
+
+			registerOnBatchComplete(handler: (batchInfo: QueryExecuteBatchNotificationParams) => any) {
+				connection.onNotification(QueryExecuteBatchCompleteNotification.type, (params: QueryExecuteBatchNotificationParams) => {
+					handler({
+						batchSummary: params.batchSummary,
+						ownerUri: params.ownerUri
+					});
+				});
+			},
+			registerOnResultSetComplete(handler: (resultSetInfo: QueryExecuteResultSetCompleteNotificationParams) => any) {
+				connection.onNotification(QueryExecuteResultSetCompleteNotification.type, (params: QueryExecuteResultSetCompleteNotificationParams) => {
+					handler({
+						ownerUri: params.ownerUri,
+    					resultSetSummary: params.resultSetSummary
+					});
+				});
+			},
+			registerOnMessage(handler: (message: QueryExecuteMessageParams) => any) {
+				connection.onNotification(QueryExecuteMessageNotification.type, (params: QueryExecuteMessageParams) => {
+					handler({
+						message: params.message,
+						ownerUri: params.ownerUri
+					});
 				});
 			}
 		};
@@ -1359,6 +1472,8 @@ export class LanguageClient {
 			capabilitiesProvider: capabilitiesProvider,
 
 			connectionProvider: connectionProvider,
+
+			queryProvider: queryProvider,
 
 			metadataProvider: metadataProvider,
 

dataprotocol-node/client/src/protocol.ts
@@ -23,7 +23,8 @@ import {
 		ConnectionSummary, ConnectionCompleteParams, IntelliSenseReadyParams,
 		ConnectionProviderOptions, DataProtocolServerCapabilities,
 		CapabiltiesDiscoveryResult, MetadataQueryParams, MetadataQueryResult,
-		ScriptingSelectParams, ScriptingSelectResult
+		ScriptingSelectParams, ScriptingSelectResult,
+		BatchSummary, QueryExecuteBatchNotificationParams, ResultSetSummary, IResultMessage, ISelectionData
 	} from 'dataprotocol-languageserver-types';
 
 
@@ -1034,6 +1035,116 @@ export namespace CapabiltiesDiscoveryRequest {
     export const type: RequestType<CapabiltiesDiscoveryParams, CapabiltiesDiscoveryResult, void> = { get method(): string { return 'capabilities/list'; } };
 }
 
+// ------------------------------- < Query Cancellation Request > ------------------------------------
+export namespace QueryCancelRequest {
+    export const type: RequestType<QueryCancelParams, QueryCancelResult, void> = { get method(): string { return 'query/cancel'; } };
+}
+
+export interface QueryCancelParams {
+    ownerUri: string;
+}
+
+export interface QueryCancelResult {
+    messages: string;
+}
+
+// ------------------------------- < Query Dispose Request > ------------------------------------
+
+export namespace QueryDisposeRequest {
+    export const type: RequestType<QueryDisposeParams, QueryDisposeResult, void> = { get method(): string { return 'query/dispose'; } };
+}
+
+/**
+ * Parameters to provide when disposing of a query
+ */
+export interface QueryDisposeParams {
+    ownerUri: string;
+}
+
+/**
+ * Result received upon successful disposal of a query
+ */
+export interface QueryDisposeResult {
+}
+
+// ------------------------------- < Query Execution Complete Notification > ------------------------------------
+export namespace QueryExecuteCompleteNotification {
+    export const type: NotificationType<QueryExecuteCompleteNotificationResult> = { get method(): string { return 'query/complete'; } };
+}
+
+/**
+ * Result received upon successful execution of a query
+ */
+export interface QueryExecuteCompleteNotificationResult {
+    ownerUri: string;
+    batchSummaries: BatchSummary[];
+}
+
+// ------------------------------- < Query Batch Start  Notification > ------------------------------------
+export namespace QueryExecuteBatchStartNotification {
+    export const type: NotificationType<QueryExecuteBatchNotificationParams> = { get method(): string { return 'query/batchStart'; } };
+}
+
+// ------------------------------- < Query Batch Complete Notification > ------------------------------------
+export namespace QueryExecuteBatchCompleteNotification {
+    export const type: NotificationType<QueryExecuteBatchNotificationParams> = { get method(): string { return 'query/batchComplete'; } };
+}
+
+// ------------------------------- < Query ResultSet Complete Notification > ------------------------------------
+export namespace QueryExecuteResultSetCompleteNotification {
+    export const type: NotificationType<QueryExecuteResultSetCompleteNotificationParams> = {  get method(): string { return 'query/resultSetComplete'; } };
+}
+
+export interface QueryExecuteResultSetCompleteNotificationParams {
+    resultSetSummary: ResultSetSummary;
+    ownerUri: string;
+}
+
+// ------------------------------- < Query Message Notification > ------------------------------------
+export namespace QueryExecuteMessageNotification {
+    export const type: NotificationType<QueryExecuteMessageParams> = { get method(): string { return 'query/message'; } };
+}
+
+export class QueryExecuteMessageParams {
+    message: IResultMessage;
+    ownerUri: string;
+}
+
+// ------------------------------- < Query Execution Request > ------------------------------------
+export namespace QueryExecuteRequest {
+    export const type: RequestType<QueryExecuteParams, QueryExecuteResult, void> = { get method(): string { return 'query/executeDocumentSelection'; } };
+}
+
+export interface QueryExecuteParams {
+    ownerUri: string;
+    querySelection: ISelectionData;
+}
+
+export interface QueryExecuteResult {}
+
+// ------------------------------- < Query Results Request > ------------------------------------
+export namespace QueryExecuteSubsetRequest {
+    export const type: RequestType<QueryExecuteSubsetParams, QueryExecuteSubsetResult, void> = { get method(): string { return 'query/subset'; } };
+}
+
+export interface QueryExecuteSubsetParams {
+    ownerUri: string;
+    batchIndex: number;
+    resultSetIndex: number;
+    rowsStartIndex: number;
+    rowsCount: number;
+}
+
+export interface ResultSetSubset {
+    rowCount: number;
+    rows: any[][];
+}
+
+export interface QueryExecuteSubsetResult {
+    message: string;
+    resultSubset: ResultSetSubset;
+}
+
 // ------------------------------- < Metadata Events > ------------------------------------
 
 export namespace MetadataQueryRequest {
@@ -1044,4 +1155,5 @@ export namespace MetadataQueryRequest {
 
 export namespace ScriptingSelectRequest {
     export const type: RequestType<ScriptingSelectParams, ScriptingSelectResult, void> = { get method(): string { return 'scripting/select'; } };
+
 }

commit a026649c39945ed19edb57d0effe9a653676543a

Add proper mock to queryEditor.test to fix broken test  (#262)
Fixing test failure caused by 1f5ff82c3c9fdedf99d940a5e91adca0749b5e5f.

src/sql/parts/query/execution/queryModel.ts
@@ -35,5 +35,5 @@ export interface IQueryModelService {
 	showError(uri: string, message: string): void;
 
 	onRunQueryStart: Event<string>;
-onRunQueryComplete: Event<string>;
+	onRunQueryComplete: Event<string>;
 }

src/sql/parts/query/execution/queryModelService.ts
@@ -11,7 +11,6 @@ import QueryRunner from 'sql/parts/query/execution/queryRunner';
 import { DataService } from 'sql/parts/grid/services/dataService';
 import { ISlickRange } from 'angular2-slickgrid';
 import { IQueryModelService } from 'sql/parts/query/execution/queryModel';
-import { IQueryManagementService } from 'sql/parts/query/common/queryManagement';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { IMessageService } from 'vs/platform/message/common/message';
 import Severity from 'vs/base/common/severity';
@@ -35,7 +34,7 @@ class QueryInfo {
 	// via the data service will be lost.
 	public dataServiceReady: boolean;
 
-	constructor () {
+	constructor() {
 		this.dataServiceReady = false;
 		this.queryEventQueue = [];
 	}
@@ -58,7 +57,6 @@ export class QueryModelService implements IQueryModelService {
 	// CONSTRUCTOR /////////////////////////////////////////////////////////
 	constructor(
 		@IInstantiationService private _instantiationService: IInstantiationService,
-		@IQueryManagementService private _queryManagementService: IQueryManagementService,
 		@IMessageService private _messageService: IMessageService
 	) {
 		this._queryInfoMap = new Map<string, QueryInfo>();

src/sqltest/parts/query/editor/queryEditor.test.ts
@@ -8,13 +8,13 @@
 import { EditorDescriptorService } from 'sql/parts/query/editor/editorDescriptorService';
 import { InstantiationService } from 'vs/platform/instantiation/common/instantiationService';
 import { IMessageService } from 'vs/platform/message/common/message';
+import { TestMessageService } from 'vs/workbench/test/workbenchTestServices';
 import { IEditorDescriptor, EditorInput } from 'vs/workbench/common/editor';
 import { TPromise } from 'vs/base/common/winjs.base';
 import URI from 'vs/base/common/uri';
 import { QueryResultsInput } from 'sql/parts/query/common/queryResultsInput';
 import { QueryEditor } from 'sql/parts/query/editor/queryEditor';
 import { QueryModelService } from 'sql/parts/query/execution/queryModelService';
-import { IQueryManagementService } from 'sql/parts/query/common/queryManagement';
 import { QueryInput } from 'sql/parts/query/common/queryInput';
 import { UntitledEditorInput } from 'vs/workbench/common/editor/untitledEditorInput';
 import { Builder } from 'vs/base/browser/builder';
@@ -25,7 +25,6 @@ import * as assert from 'assert';
 suite('SQL QueryEditor Tests', () => {
 	let queryModelService: QueryModelService;
 	let instantiationService: TypeMoq.Mock<InstantiationService>;
-	let queryManagementService: TypeMoq.Mock<IQueryManagementService>;
 	let messageService: TypeMoq.Mock<IMessageService>;
 	let editorDescriptorService: TypeMoq.Mock<EditorDescriptorService>;
 	let queryInput: QueryInput;
@@ -81,8 +80,11 @@ suite('SQL QueryEditor Tests', () => {
 		let queryResultsInput2: QueryResultsInput = new QueryResultsInput(uri2.fsPath);
 		queryInput2 = new QueryInput('second', 'second', fileInput2, queryResultsInput2, undefined);
 
+		// Mock IMessageService
+		messageService = TypeMoq.Mock.ofType(TestMessageService, TypeMoq.MockBehavior.Loose);
+
 		// Create a QueryModelService
-		queryModelService = new QueryModelService(instantiationService.object, queryManagementService.object, messageService.object);
+		queryModelService = new QueryModelService(instantiationService.object, messageService.object);
 	});
 
 	test('createEditor creates only the taskbar', (done) => {