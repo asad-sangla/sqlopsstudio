7f8418d04e8a1ff9785666a48031ef11419fc774
kburtram <karlb@microsoft.com>
2017-03-15 16:54:48 -0700

    Fix missing data namespace

--- src/vs/workbench/api/node/extHostExtensionService.ts
+++ src/vs/workbench/api/node/extHostExtensionService.ts
@@ -13,7 +13,7 @@ import { AbstractExtensionService, ActivatedExtension } from 'vs/platform/extens
 import { IExtensionDescription } from 'vs/platform/extensions/common/extensions';
 import { ExtHostStorage } from 'vs/workbench/api/node/extHostStorage';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
-import { createApiFactory, initializeExtensionApi } from 'vs/workbench/api/node/extHost.api.impl';
+import { createApiFactory, initializeExtensionApi } from 'sql/workbench/api/node/sqlExtHost.api.impl';
 import { IThreadService } from 'vs/workbench/services/thread/common/threadService';
 import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
 import { MainContext, MainProcessExtensionServiceShape, IEnvironment, IInitData } from './extHost.protocol';
