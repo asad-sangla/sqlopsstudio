f772dda2bd1ba695b5e1d38b6dba0a37a8964129
Aditya Bist <adbist@microsoft.com>
2017-04-05 12:07:09 -0700

    added resources for carbon to out-build (#530)

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -77,6 +77,9 @@ const vscodeResources = [
 	'out-build/vs/workbench/services/files/**/*.md',
 	'out-build/sql/parts/grid/directives/{slick.dragrowselector.js,slick.autosizecolumn.js}',
 	'out-build/sql/parts/grid/load/loadJquery.js',
+	'out-build/sql/parts/connection/dashboard/**/*.html',
+	'out-build/sql/parts/connection/connectionDialog/media/**',
+	'out-build/sql/parts/connection/dashboard/media/**',
 	'!**/test/**'
 ];
 
