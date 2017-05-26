59ebb00947f40a26c08a8f079651ffd554b67bff
Leila Lali <llali@microsoft.com>
2017-05-26 11:58:08 -0700

    fixed the exception when opening editor from Oe (#951)
    * fixed the exception when opening editor from Oe

--- src/sql/common/taskUtilities.ts
+++ src/sql/common/taskUtilities.ts
@@ -5,6 +5,7 @@
 'use strict';
 
 import { IConnectionProfile } from 'sql/parts/connection/common/interfaces';
+import { ConnectionProfile } from 'sql/parts/connection/common/connectionProfile';
 import {
 	IConnectableInput, IConnectionManagementService,
 	IConnectionCompletionOptions, ConnectionType
@@ -20,26 +21,59 @@ import data = require('data');
 
 export class TaskUtilities {
 
+	private static connectIfNotAlreadyConnected(connectionProfile: ConnectionProfile, connectionService: IConnectionManagementService): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			let uri = connectionService.getConnectionId(connectionProfile);
+			if (!connectionService.isConnected(uri)) {
+				let options: IConnectionCompletionOptions = {
+					params: { connectionType: ConnectionType.editor, runQueryOnCompletion: true, input: undefined },
+					saveTheConnection: false,
+					showDashboard: false,
+					showConnectionDialogOnError: false
+				};
+				connectionService.connect(connectionProfile, uri, options).then(() => {
+				setTimeout(function(){
+        			resolve();
+   				 }, 2000);
+				}).catch(connectionError => {
+					reject(connectionError);
+				});
+			} else {
+				resolve();
+			}
+		});
+	}
+
 	/**
 	 * Select the top rows from an object
 	 */
-	public static scriptSelect(connectionProfile: IConnectionProfile, metadata: data.ObjectMetadata, ownerUri: string, connectionService: IConnectionManagementService, queryEditorService: IQueryEditorService, scriptingService: IScriptingService): Promise<void> {
-		return new Promise<void>((resolve) => {
-			scriptingService.scriptAsSelect(ownerUri, metadata).then(result => {
-				if (result && result.script) {
-					queryEditorService.newSqlEditor(result.script).then((owner: IConnectableInput) => {
-						// Connect our editor to the input connection
-						let options: IConnectionCompletionOptions = {
-							params: { connectionType: ConnectionType.editor, runQueryOnCompletion: true, input: owner },
-							saveTheConnection: false,
-							showDashboard: false,
-							showConnectionDialogOnError: true
-						};
-						connectionService.connect(connectionProfile, owner.uri, options).then(() => {
-							resolve();
+	public static scriptSelect(connectionProfile: ConnectionProfile, metadata: data.ObjectMetadata, ownerUri: string, connectionService: IConnectionManagementService, queryEditorService: IQueryEditorService, scriptingService: IScriptingService): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			TaskUtilities.connectIfNotAlreadyConnected(connectionProfile, connectionService).then(connectionResult => {
+				scriptingService.scriptAsSelect(ownerUri, metadata).then(result => {
+					if (result && result.script) {
+						queryEditorService.newSqlEditor(result.script).then((owner: IConnectableInput) => {
+							// Connect our editor to the input connection
+							let options: IConnectionCompletionOptions = {
+								params: { connectionType: ConnectionType.editor, runQueryOnCompletion: true, input: owner },
+								saveTheConnection: false,
+								showDashboard: false,
+								showConnectionDialogOnError: true
+							};
+							connectionService.connect(connectionProfile, owner.uri, options).then(() => {
+								resolve();
+							});
+						}).catch(edirotError => {
+							reject(edirotError);
 						});
-					});
-				}
+					} else {
+						reject('Scripting service did not return anything');
+					}
+				}, scriptError => {
+					reject(scriptError);
+				});
+			}).catch(connectionError => {
+				reject(connectionError);
 			});
 		});
 	}
@@ -67,19 +101,29 @@ export class TaskUtilities {
 	/**
 	 * Script the object as a CREATE statement
 	 */
-	public static scriptCreate(metadata: data.ObjectMetadata, ownerUri: string, queryEditorService: IQueryEditorService, scriptingService: IScriptingService): Promise<void> {
-		return new Promise<void>((resolve) => {
-			scriptingService.scriptAsCreate(ownerUri, metadata).then(result => {
-				if (result && result.script) {
-					let script = result.script;
-					var startPos: number = script.toLowerCase().indexOf('create');
-					if (startPos > 0) {
-						script = script.substring(startPos);
+	public static scriptCreate(connectionProfile: ConnectionProfile, metadata: data.ObjectMetadata, ownerUri: string, connectionService: IConnectionManagementService, queryEditorService: IQueryEditorService, scriptingService: IScriptingService): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			TaskUtilities.connectIfNotAlreadyConnected(connectionProfile, connectionService).then(connectionResult => {
+				scriptingService.scriptAsCreate(ownerUri, metadata).then(result => {
+					if (result && result.script) {
+						let script = result.script;
+						var startPos: number = script.toLowerCase().indexOf('create');
+						if (startPos > 0) {
+							script = script.substring(startPos);
+						}
+						queryEditorService.newSqlEditor(script).then(() => {
+							resolve();
+						}).catch(editorError => {
+							reject(editorError);
+						});
+					} else {
+						reject('Scripting service did not return anything');
 					}
-					queryEditorService.newSqlEditor(script).then(() => {
-						resolve();
-					});
-				}
+				}, scriptingError => {
+					reject(scriptingError);
+				});
+			}).catch(connectionError => {
+				reject(connectionError);
 			});
 		});
 	}
