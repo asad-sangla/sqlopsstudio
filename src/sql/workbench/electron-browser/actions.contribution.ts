e3fbfa19048b36d186e4b5ca4c773dcb25d8fe45
Karl Burtram <karlb@microsoft.com>
2017-09-06 14:51:14 -0700

    Remove Create Database icon from dashboard (#1842)

--- src/sql/workbench/electron-browser/actions.contribution.ts
+++ src/sql/workbench/electron-browser/actions.contribution.ts
@@ -26,13 +26,6 @@ const newQuerySchema: IJSONSchema = {
 	default: null
 };
 
-const newDatabaseSchema: IJSONSchema = {
-	description: nls.localize('carbon.actions.newDatabase', 'Open up new database Dialog'),
-	type: 'null',
-	default: null
-};
-
 registerTask('backup', '', backupSchema, Actions.BackupAction);
 registerTask('restore', '', restoreSchema, Actions.RestoreAction);
 registerTask('new-query', '', newQuerySchema, Actions.NewQueryAction);
-registerTask('new-database', '', newDatabaseSchema, Actions.NewDatabaseAction);
