df81d8d08f76bc084451a64eb59e06da4c39b9ac
Abbie Petchtes <appetcht@microsoft.com>
2017-11-29 12:55:56 -0800

    Add azure command palette (#3043)
    * add command palette for opening the account management dialog
    
    * formatting

---
+++ src/sql/parts/accountManagement/common/accountManagement.contribution.ts
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import { Registry } from 'vs/platform/registry/common/platform';
+import { IWorkbenchActionRegistry, Extensions } from 'vs/workbench/common/actionRegistry';
+import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
+
+import { ManageLinkedAccountAction } from 'sql/parts/accountManagement/accountListStatusbar/accountListStatusbarItem';
+
+let actionRegistry = <IWorkbenchActionRegistry>Registry.as(Extensions.WorkbenchActions);
+
+actionRegistry.registerWorkbenchAction(
+	new SyncActionDescriptor(
+		ManageLinkedAccountAction,
+		ManageLinkedAccountAction.ID,
+		ManageLinkedAccountAction.LABEL
+	),
+	ManageLinkedAccountAction.LABEL
+);
--- src/vs/workbench/workbench.main.ts
+++ src/vs/workbench/workbench.main.ts
@@ -133,6 +133,7 @@ import 'sql/parts/query/common/query.contribution';
 import 'sql/parts/profiler/contrib/profiler.contribution';
 import 'sql/parts/profiler/contrib/profilerActions.contribution';
 import 'sql/parts/registeredServer/serverGroupDialog/serverGroup.contribution';
+import 'sql/parts/accountManagement/common/accountManagement.contribution';
 /* Insights */
 import 'sql/parts/dashboard/widgets/insights/views/charts/types/barChart.contribution';
 import 'sql/parts/dashboard/widgets/insights/views/charts/types/doughnutChart.contribution';
